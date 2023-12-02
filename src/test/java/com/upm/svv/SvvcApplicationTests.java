ccd04279683321ba50f5d7a16a3607314c677dbe
andrei <Your Email>
2023-12-02 22:02:46 +0100

    mnuda puta mierda de ejercicio nano

--- src/test/java/com/upm/svv/SvvcApplicationTests.java
+++ src/test/java/com/upm/svv/SvvcApplicationTests.java
@@ -1,13 +1,27 @@
 package com.upm.svv;
 
+import com.upm.svv.service.InputService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SvvcApplicationTests {
 
+    @Autowired
+    private InputService inputService;
     @Test
-    void contextLoads() {
+    void badFileInput() {
+        inputService.run(new DefaultApplicationArguments("badfile_l"));
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void goodFileInput() {
+        inputService.run(new DefaultApplicationArguments("./src/test/resources/input_file.txt"));
+        Assertions.assertTrue(true);
     }
 
 }
