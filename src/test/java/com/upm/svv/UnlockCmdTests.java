987beefbfd7d8de6e9678816af2f717851518709
andrei <Your Email>
2023-12-03 11:38:38 +0100

    unlock pwd

---
+++ src/test/java/com/upm/svv/UnlockCmdTests.java
@@ -0,0 +1,67 @@
+package com.upm.svv;
+
+import com.upm.svv.service.InputService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+@SpringBootTest
+class UnlockCmdTests {
+    @Autowired
+    private InputService inputService;
+    private final PrintStream standardOut = System.out;
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    @BeforeEach
+    public void setUp() {
+        inputService.cmdService.contactList.clear();
+        outputStreamCaptor.reset();
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+    @AfterEach
+    public void tearDown() {
+        inputService.cmdService.contactList.clear();
+        String out = outputStreamCaptor.toString();
+        outputStreamCaptor.reset();
+        System.setOut(standardOut);
+        System.out.println(out);
+    }
+    @Test
+    void unlockCmdInvalidDirShouldFail() {
+        inputService.init("./src/test/resources/UnlockCmd/invalid_dir.txt");
+        Assertions.assertTrue(outputStreamCaptor.toString().contains("the directory does not exist or it has no pwd"));
+        Assertions.assertFalse(inputService.cmdService.contactList.isEmpty());
+    }
+    @Test
+    void unlockCmdValidDirValidPwdNoPreviousPWDShouldFail() {
+        inputService.init("./src/test/resources/UnlockCmd/valid_dir_no_previous_pwd.txt");
+        Assertions.assertTrue(outputStreamCaptor.toString().contains("the directory does not exist or it has no pwd"));
+        Assertions.assertFalse(inputService.cmdService.contactList.isEmpty());
+        Assertions.assertTrue(inputService.cmdService.contactList.containsKey("usemepath"));
+        Assertions.assertNull(inputService.cmdService.contactList.get("usemepath").getPwd());
+    }
+
+    @Test
+    void unlockCmdValidDirValidPwdShouldPass() {
+        inputService.init("./src/test/resources/UnlockCmd/valid_dir_right_pwd.txt");
+        Assertions.assertTrue(outputStreamCaptor.toString().contains("The directory was unprotected"));
+        Assertions.assertFalse(inputService.cmdService.contactList.isEmpty());
+        Assertions.assertTrue(inputService.cmdService.contactList.containsKey("usemepath"));
+        Assertions.assertNull(inputService.cmdService.contactList.get("usemepath").getPwd());
+    }
+    @Test
+    void unlockCmdValidDirInvalidPwdShouldFail() {
+        inputService.init("./src/test/resources/UnlockCmd/valid_dir_invalid_pwd.txt");
+        Assertions.assertTrue(outputStreamCaptor.toString().contains("Invalid password"));
+        Assertions.assertFalse(inputService.cmdService.contactList.isEmpty());
+        Assertions.assertTrue(inputService.cmdService.contactList.containsKey("usemepath"));
+        Assertions.assertEquals("1234",inputService.cmdService.contactList.get("usemepath").getPwd());
+    }
+
+}
